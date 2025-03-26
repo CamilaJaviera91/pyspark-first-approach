f292385726a382cda06c27769b5d252c9143a0eb
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-03-26 11:37:52 -0300

    feat(analysis): change function name

--- 4. analysis.py
+++ 4. analysis.py
@@ -4,7 +4,7 @@ import shutil
 import os
 import logging
 
-def advanced_analysis(spark):
+def previous_years(spark):
     try:
         # Read CSV file into a DataFrame with header and schema inference
         df = spark.read.csv("./data/cleaned_data_output", header=True, inferSchema=True)
